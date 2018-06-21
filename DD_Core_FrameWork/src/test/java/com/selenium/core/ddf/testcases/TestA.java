3813650695c4bd0a9edde0a8d76be2843f229c2b
rameshbestha <rameshbestha@gmail.com>
2018-06-21 17:16:22 +1000

    firstcommit

---
+++ DD_Core_FrameWork/src/test/java/com/selenium/core/ddf/testcases/TestA.java
@@ -0,0 +1,18 @@
+package com.selenium.core.ddf.testcases;
+
+import org.testng.annotations.Test;
+
+public class TestA {
+
+	@Test
+	public void Testa1()
+	{
+		
+	}
+	@Test
+	public void Testa2() {
+		
+	}
+	
+	
+}
