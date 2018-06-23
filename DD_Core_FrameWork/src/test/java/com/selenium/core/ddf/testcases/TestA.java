6912cbea3870a1ae7a436d0b446029f0668cfd9a
rameshbestha <rameshbestha@gmail.com>
2018-06-23 22:05:31 +1000

    homecode2306

--- DD_Core_FrameWork/src/test/java/com/selenium/core/ddf/testcases/TestA.java
+++ DD_Core_FrameWork/src/test/java/com/selenium/core/ddf/testcases/TestA.java
@@ -1,26 +1,29 @@
 package com.selenium.core.ddf.testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TestA {
 	
-	@Test(priority = 1)
-	public void testa1() {
+	@Test(priority=1)
+	public void testA1(){
+		Assert.fail();
+	}
+	
+	@Test(priority=2,dependsOnMethods={"testA1"})
+	public void testA2(){
+		System.out.println("Second test");
 		
 	}
 	
-	@Test(priority = 2, dependsOnMethods={"testa1"})
-	public void testa2() {
+	@Test(priority=3,dependsOnMethods={"testA1","testA2"})
+	public void testA3(){
 		
 	}
 	
-	@Test(priority = 2, dependsOnMethods={"testa1","testa2"})
-	public void testa3() {
-		
-	}
 	@AfterMethod
-	public void quit() {
+	public void quit(){
 		
 	}
 	
